f3852754daf8a102f6b364edd3124d346c54f554
badu01 <badushapareeth777@gmail.com>
2024-03-24 07:50:02 +0530

    Bus search done

--- lib/Constants/Constants.dart
+++ lib/Constants/Constants.dart
@@ -1,5 +1,6 @@
   List<String> suggestions = [];
+    List<String> Bussuggestions = [];
   List BusData = [];
   bool loading = false;
-
+  var Busdetails;
   var C_User=null;
--- lib/Constants/Controllers.dart
+++ lib/Constants/Controllers.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 TextEditingController FromController = TextEditingController();
 TextEditingController ToController = TextEditingController();
+
+TextEditingController BusController = TextEditingController();
